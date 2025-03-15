2d567e7c2018195677edc00104fc0425bfcebb69
Maarten van Stam <maarten@vanstam.org>
2025-03-15 22:05:01 +0100

    Add SayHello method and update PowerPoint integration
    - ✨ Introduced `SayHello` method in `Home.razor.cs` for greeting.
    - 🆕 Added `callBlazorOnHome` function in `commands.js` to create text boxes in PowerPoint slides.
    - 🔄 Updated Office manifest to link new function and changed button labels/tooltips.
    
    - MvS

--- Blazor.PowerPoint.AddIn/Blazor.PowerPoint.AddIn.Client/wwwroot/commands/commands.js
+++ Blazor.PowerPoint.AddIn/Blazor.PowerPoint.AddIn.Client/wwwroot/commands/commands.js
@@ -36,5 +36,60 @@ async function insertTextInPowerPoint(event) {
     event.completed();
 }
 
+/**
+ * Writes the text from the Home Blazor Page to the PowerPoint slide
+ * @param {any} event
+ */
+async function callBlazorOnHome(event) {
+    console.log("Running callBlazorOnHome");
+
+
+    try {
+        let name = "Initializing";
+
+        try {
+
+            //--------------------------------------------------------------------------------
+            // TODO: Dotnet seems to be undefined on first run ... need to investigate
+            //--------------------------------------------------------------------------------
+            try {
+
+                // Call JSInvokable Function here ...
+                name = await DotNet.invokeMethodAsync("Blazor.PowerPoint.AddIn.Client", "SayHello", "Blazor Fan");
+
+            } catch (err) {
+                name = err.message;
+                console.error("Error during DotNet invocation: " + err.message);
+            }
+
+            console.log("Finished Initializing: " + name)
+        }
+        catch (err) {
+            name = err.message;
+        }
+
+        await PowerPoint.run(async (context) => {
+            const slide = context.presentation.getSelectedSlides().getItemAt(0);
+            const textBox = slide.shapes.addTextBox(name);
+            textBox.fill.setSolidColor("white");
+            textBox.lineFormat.color = "black";
+            textBox.lineFormat.weight = 1;
+            textBox.lineFormat.dashStyle = PowerPoint.ShapeLineDashStyle.solid;
+            await context.sync();
+        });
+    }
+    catch (err) {
+        console.log();
+        console.log("Error call : " + err.message);
+    }
+    finally {
+        console.log("Finish callBlazorOnHome");
+    }
+
+    // Calling event.completed is required. event.completed lets the platform know that processing has completed.
+    event.completed();
+}
+
 // Associate the functions with their named counterparts in the manifest XML.
-Office.actions.associate("action", insertTextInPowerPoint);
+Office.actions.associate("insertTextInPowerPoint", insertTextInPowerPoint);
+Office.actions.associate("callBlazorOnHome", callBlazorOnHome);
